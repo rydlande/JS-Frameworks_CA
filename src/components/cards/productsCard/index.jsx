9718b9de23151e070455e82c856e83920fd27934
rydlande <rydlande@hotmail.com>
2024-04-07 12:50:13 +0200

    changed size on products card

--- src/components/cards/productsCard/index.jsx
+++ src/components/cards/productsCard/index.jsx
@@ -11,7 +11,7 @@ export function ProductsCard({ products }) {
                 <img 
                   src={product.image.url} 
                   alt={product.title} 
-                  className="block m-auto object-cover rounded-sm w-36 h-36 md:max-h-60 md:max-w-52 lg:max-h-72 lg:max-w-64 lg:rounded-lg"/>
+                  className="block m-auto object-cover rounded-sm w-36 h-44 md:max-h-60 md:max-w-52 lg:max-h-72 lg:max-w-64 lg:rounded-lg"/>
                 <h2 className='sfont-semibold text-sm mt-1 md:text-base md:mt-6 lg:text-3xl lg:mt-8'>{product.title}</h2>
                 <p className='font-light text-xs mt-0 md:text-base md:mt-4 lg:text-2xl lg:mt-6'>{product.discountedPrice} NOK</p>
               </div>
