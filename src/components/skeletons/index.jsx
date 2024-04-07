5caaec0a8569c45023b895d0622e978a24ed4fbb
rydlande <rydlande@hotmail.com>
2024-04-07 23:56:22 +0200

    glemte å lagre en fil

--- src/components/skeletons/index.jsx
+++ src/components/skeletons/index.jsx
@@ -1,5 +1,4 @@
 import { ProductsSkeleton } from "./productsSkeleton";
 import { SpecificSkeleton } from "./specificSkeleton";
-import { CartSkeleton } from "./cartSkeleton";
 
-export { ProductsSkeleton, SpecificSkeleton, CartSkeleton };
+export { ProductsSkeleton, SpecificSkeleton };
