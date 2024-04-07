0bdb7eeedf0ee1021a72e2d61f877a2be9fc6bfa
rydlande <rydlande@hotmail.com>
2024-04-07 15:32:07 +0200

    test

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,7 @@
 import { Header } from "./header";
 import { Footer } from "./footer";
-//import { Loader } from "./loader";
+import { Loader } from "./loader";
 import { CartCard, CheckoutSuccessCard, ProductsCard, SpecificCard } from "./cards";
 
 
-export { Header, Footer, CartCard, CheckoutSuccessCard, ProductsCard, SpecificCard };
+export { Header, Footer, CartCard, CheckoutSuccessCard, ProductsCard, SpecificCard, Loader };
